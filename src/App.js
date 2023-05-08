9a4602ae8077b5888d7f225a93997ab74d10a4dc
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-05-08 18:10:16 -0400

    initial starting point for App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
-    <div className="App">
+    <div>
+
     </div>
   );
 }
